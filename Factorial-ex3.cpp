8d8fd440bf6981239f6b8d4df3f148c0e5105863
beglesto <143560574+beglesto@users.noreply.github.com>
2025-02-26 18:24:54 +0000

    completed ex2 and ex3

--- Factorial-ex3.cpp
+++ Factorial-ex3.cpp
@@ -1,1 +1,15 @@
 // Add your code here
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for(int i=1; i<=10; i++) {
+        int fact = 1;
+        for(int j=i; j>0; j--) {
+            fact*=j;
+        }
+        cout << "factorial of " << i << " is " << fact << "\n";
+    }
+    return 0;
+}
