5dddd015b13bad91ddad18a13f1295dce427989f
beglesto <143560574+beglesto@users.noreply.github.com>
2025-02-26 18:36:10 +0000

    ex5 complete

--- UserInput-ex5.cpp
+++ UserInput-ex5.cpp
@@ -1,1 +1,22 @@
 // Add your code here
+#include <iostream>
+
+using namespace std;
+
+int factorial(int num) {
+    int fact = 1;
+    for(int j=num; j>0; j--) {
+        fact*=j;
+    }
+    return fact;
+}
+
+int main() {
+    int userin;
+    cout << "Please enter an integer value: ";
+    cin >> userin;
+    int out = factorial(userin);
+    cout << "The value you entered is " << userin; 
+    cout << " and its factorial is " << out << "\n"; 
+    return 0;
+}
