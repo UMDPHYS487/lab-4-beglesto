d7e84abf263a9a634a31a4bb2c1cd3683811a337
beglesto <143560574+beglesto@users.noreply.github.com>
2025-02-26 18:55:20 +0000

    ex6 complete

--- SwitchCase-ex7.cpp
+++ SwitchCase-ex7.cpp
@@ -1,1 +1,28 @@
 // add your code here
+#include <iostream>
+using namespace std;
+
+int main() {
+    int inp;
+    cout << "Please enter an integer: ";
+    cin >> inp;
+    switch (inp % 2) {
+        case 0:
+            switch(inp) {
+                case 0:
+                case 2:
+                case 4:
+                case 6:
+                case 8:
+                    cout << inp*inp << "\n";
+                    break;
+                default:
+                    cout << "hahaha\n";
+            }
+        case 1:
+            cout << float(inp)/2 << "\n";
+            break; 
+    }
+
+    return 0;
+}
--- cin-ex6.cpp
+++ cin-ex6.cpp
@@ -1,1 +1,17 @@
 // Add your code here
+#include <iostream>
+using namespace std;
+
+int y;
+int *x;
+
+int main() {
+    y = 4;
+    x = &y;
+    cout << *x << "\n";
+    y = 16;
+    cout << *x << "\n";
+    y = 256;
+    cout << *x << "\n";
+    return 0;
+}
