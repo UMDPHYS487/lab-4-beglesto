a374154d6bd8c56eb8cdea09fae5b5de3bf8755e
beglesto <143560574+beglesto@users.noreply.github.com>
2025-02-26 18:29:43 +0000

    ex4 complete

--- FactorialLoop-ex4.cpp
+++ FactorialLoop-ex4.cpp
@@ -1,1 +1,20 @@
 // Add your code here
+#include <iostream>
+
+using namespace std;
+
+int factorial(int num) {
+    int fact = 1;
+    for(int j=num; j>0; j--) {
+        fact*=j;
+    }
+    return fact;
+}
+
+int main() {
+    for(int i=1; i<=10; i++) {
+        int fact = factorial(i);
+        cout << "factorial of " << i << " is " << fact << "\n";
+    }
+    return 0;
+}
